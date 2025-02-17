729140bd899f9b51f91159397fe301a0041dd199
Meraki <130898314+sd28-k@users.noreply.github.com>
2025-02-17 21:38:27 +0530

    Create logintovCenter.js

---
+++ logintovCenter.js
@@ -0,0 +1,5 @@
+//Login to Vcenter
+var serviceInstance = new VcServiceInstance(vCenterServer, vCenterUsername, vCenterPassword);
+System.log("Successfully logged into vCenter: " + vCenterServer);
+
+//Set the Variables
